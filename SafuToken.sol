c8e6c8b2b4d32cbd3dc1b992d98aa767fd47e834
safu-protocol <84081868+safu-protocol@users.noreply.github.com>
2022-09-08 17:08:22 +0100

    Update SafuToken.sol

--- SafuToken.sol
+++ SafuToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SafuToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Safu Token", "SAFU") {
+    constructor(uint256 initialSupply) ERC20("Safu Protocol", "SAFU") {
         _mint(msg.sender, initialSupply);
     }
 }
